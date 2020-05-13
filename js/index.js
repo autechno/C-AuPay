8d62592965782d224573165f2dd79db46c9d861a
noad.zhu <noah.zhu@austintechno.com>
2020-05-13 11:46:29 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -9,65 +9,53 @@ $(document).ready(function(){
 })
 
 // 公链和币种数据
-var chainList = [
-  {
-    name: 'TRC20',
-    id: 1,
-    coinList: [
-      { name: 'BTC', id: '1.1' },
-      { name: 'BTC1', id: '1.2' },
-      { name: 'BTC2', id: '1.3' }
-    ]
-  },
-  {
-    name: 'OMNI',
-    id: 2,
-    coinList: [
-      { name: 'ETH', id: '2.1', min: 10, max: 500 },
-      { name: 'ETH1', id: '2.2', min: 18, max: 510 },
-      { name: 'ETH2', id: '2.3', min: 109, max: 509 }
-    ]
-  }
+var currencyList =  [
+  { label: 'BTC', value: 1, chain: [{ label: 'ERC20', value: 2 }, { label: 'ERC201', value: 1 }]},
+  { label: 'ETH', value: 2, chain: [{ label: 'ERC20', value: 1 }] },
+  { label: 'USDT', value: 3, chain: [{ label: 'ERC201', value: 1 }] },
+  { label: 'LTC', value: 5, chain: [{ label: 'ERC20', value: 1 }] },
+  { label: 'EOS', value: 7, chain: [{ label: 'ERC20', value: 1 }] },
+  { label: 'BCH', value: 8, chain: [{ label: 'ERC20', value: 1 }] }
 ]
-var chainCur = null, // 当前选中 公链 
-    coinCur = null, // 当前选中 币种
-    chainTxt = null, // 当前选中 公链文字
-    coinTxt = null // 当前选中 币种文字
+var coinCur = null, // 当前选中 币种
+    chainCur = null, // 当前选中 公链 
+    coinTxt = null, // 当前选中 币种文字
+    chainTxt = null // 当前选中 公链文字
 // 默认转币方式
 $('input:radio').eq(0).attr('checked', 'true')
 // 点击 选择币种 弹出弹窗
 $('#coinList').click(function() {
   $("#transitionExample").popup("open")
-  if (!chainList || !chainList.length) return
-  templateEngine(chainList, '#popChainList', 'chain_i', 'data-chain', chainOperCur)
+  if (!currencyList || !currencyList.length) return
+  templateEngine(currencyList, '#popCoinList', 'coin_i', 'data-coin', chainOperCur)
 })
 $('#payType').click(function() {
   $('#coinList').click()
 })
-// 公链列表赋值以及给予选中状态
+// 币种列表赋值以及给予选中状态
 function chainOperCur(id) {
-  $('.chain_i').each(function(v, i) {
-    if ($(this).attr('data-chain') === id) {
+  $('.coin_i').each(function(v, i) {
+    if ($(this).attr('data-coin') === id) {
       $(this).addClass('cur').siblings().removeClass('cur')
     }
   })
 }
 
-// 点击某一个公链
-$(document).on('click', '.chain_i', function() {
-  $('.chain_i').off("click")
+// 点击某一个币种
+$(document).on('click', '.coin_i', function() {
+  $('.coin_i').off("click")
   $(this).addClass('cur').siblings().removeClass('cur')
-  const chainId = $(this).attr('data-chain')
-  chainCur = chainId
-  if (!chainId) return false
-  chainUpdataCoin(chainId)
+  const id = $(this).attr('data-coin')
+  coinCur = id
+  if (!id) return false
+  chainUpdataCoin(id)
   return false
 })
-// 点击公链后 币种要随之变化
-function chainUpdataCoin(chainId) {
-  $.each(chainList, function(i, v){
-    if(String(v.id) === chainId) {
-      templateEngine(v.coinList, '#popCoinList', 'coin_i', 'data-coin')
+// 点击币种后 公链要随之变化
+function chainUpdataCoin(id) {
+  $.each(currencyList, function(i, v){
+    if(String(v.value) === id) {
+      templateEngine(v.chain, '#popChainList', 'chain_i', 'data-chain')
     }
   })
 }
@@ -76,27 +64,28 @@ function templateEngine (arr, id, labelClass, labelAttr, cb) {
   let tempHtml = '' // 公链的列表
   for(let i = 0, leng = arr.length; i < leng; i ++) {
     let item = arr[i]
-    tempHtml += `<li class="${labelClass}" ${labelAttr}="${item.id}">${item.name}<li/>`
+    tempHtml += `<li class="${labelClass}" ${labelAttr}="${item.value}">${item.label}<li/>`
   }
   $(id).html(tempHtml)
-  if (cb && chainCur) cb(chainCur)
+  if (cb && coinCur) cb(coinCur)
 }
 
-// 选择好了币（关闭弹窗等操作）
-$(document).on('click', '.coin_i', function() {
+// 选择好了公链（关闭弹窗等操作）
+$(document).on('click', '.chain_i', function() {
   $(this).addClass('cur').siblings().removeClass('cur')
-  coinCur = $(this).attr('data-coin')
-  if (!$('.chain_i.cur').html()) return alert('请选择公链')
+  chainCur = $(this).attr('data-chain')
   if (!$('.coin_i.cur').html()) return alert('请选择币种')
+  if (!$('.chain_i.cur').html()) return alert('请选择公链')
   chainTxt = $('.chain_i.cur').html()
   coinTxt = $('.coin_i.cur').html()
-  $('#payType').val(chainTxt + '/' + coinTxt)
+  $('#payType').val(coinTxt + '/' + chainTxt)
   $("#transitionExample").popup("close")
   $('.error_prompt').hide()
 })
 
 // 下一步
 $('.nextStep').click(function() {
+  console.log(chainCur, coinCur)
   if (!$('#depositMoney').val()) return $('.form_i .error_prompt').eq(0).css('display', 'block')
   if (!chainCur || !coinCur) return $('.form_i .error_prompt').eq(1).css('display', 'block')
   if (!$('#payNum').val()) return $('.form_i .error_prompt').eq(2).css('display', 'block')
